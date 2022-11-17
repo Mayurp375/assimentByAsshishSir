c7448ded0e5ac9f4491c33bd0339b02574113eb5
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-11-17 20:09:56 +0530

    duplicate charector string prongram

--- duplicateCharectorInString.java
+++ duplicateCharectorInString.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class duplicateCharectorInString {
-//
+    //
     static char ch[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'a', 'b', 'c', 'd' };
 
     public static void main(String[] args) {
+        duplicateCharectorInString obj = new duplicateCharectorInString();
+        // obj.duplicateChar();       //duplicate Charector in String array
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter string name");
+        String name = sc.nextLine();
+        obj.duplicateCharectorString(name);
+    }
+
+    public void duplicateChar() {
         for (int j = 0; j < ch.length; j++) {
             for (int i = j + 1; i < ch.length; i++) {
                 if (ch[j] == ch[i]) {
@@ -11,4 +22,18 @@ public class duplicateCharectorInString {
             }
         }
     }
+
+    public static void duplicateCharectorString(String name) {
+
+        for (int i = 0; i < name.charAt(i); i++) {
+            for (int j = i + 1; j < name.charAt(j); j++) {
+                if (name.charAt(i) == name.charAt(j)) {
+                    System.out.println(name.charAt(j) + " " + "is the duplicate charector in string");
+                } else {
+                    System.out.println("no nublicate charector in string");
+                }
+            }
+        }
+    }
+
 }
